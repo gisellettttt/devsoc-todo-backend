c14c3ac53e9e410b42ee9bd6ec71771936ce6f60
Aryaman <aryaman.sakthi@gmail.com>
2025-07-12 15:14:48 +1000

    Modules for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './app.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // For actual projects this is not safe, instead use db migrations
+      synchronize: true,   // Since this is an example project so keeping this true is fine
+      
+      ssl: {
+        rejectUnauthorized: false,  // Required for Neon DB
+      },
+    }),
+    TypeOrmModule.forFeature([Todo]),   // Register Todo entity
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
