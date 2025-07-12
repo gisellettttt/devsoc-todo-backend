3cc6a2a2676e1c34c2cb64065da8f4c79506f187
Aryaman <aryaman.sakthi@gmail.com>
2025-07-12 15:15:16 +1000

    Added todo list functions

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './app.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepo: Repository<Todo>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async addTodo(desc: string): Promise<Todo> {
+    const todo = this.todoRepo.create({ desc });
+    return await this.todoRepo.save(todo);
+  }
+
+  async deleteTodo(id: number): Promise<void> {
+    await this.todoRepo.delete(id);
+  }
+
+  async toggleComplete(id: number): Promise<Todo> {
+    const todo = await this.todoRepo.findOne({ where: {id} });
+
+    if (!todo) {
+      throw new Error('Todo not fount');
+    }
+
+    todo.completed = !todo.completed;
+    return await this.todoRepo.save(todo);
+  }
+
+  async getAllTodos(): Promise<Todo[]> {
+    return await this.todoRepo.find();
   }
 }
