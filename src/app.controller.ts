7d2647f937ada98298230624fcc1542b78c52d7c
Aryaman <aryaman.sakthi@gmail.com>
2025-07-12 15:15:45 +1000

    Implemented all http methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('todos')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  async addTodo(@Body() body: { desc: string }) {
+    if (!body.desc || body.desc.trim() === '') {
+      throw new HttpException('The Todo desc cannot be empty', HttpStatus.BAD_REQUEST);
+    }
+
+    return await this.appService.addTodo(body.desc);
+  }
+
+  @Delete(':id')
+  async deleteTodo(@Param('id') id: number) {
+    return await this.appService.deleteTodo(id);
+  }
+
+  @Put(':id/complete')
+  async toggleComplete(@Param('id') id: number) {
+    return await this.appService.toggleComplete(id);
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getAllTodos() {
+    return await this.appService.getAllTodos();
   }
 }
